519845c38d6a284f613c52a657358e0e705cff2c
Mateusz Dybich <mateuszdybich1@gmail.com>
2023-08-17 01:29:27 +0200

    Create EmployeeTasks Controller

---
+++ CollabDo.Web/Controllers/EmployeeTasksController.cs
@@ -0,0 +1,49 @@
+﻿
+using CollabDo.Application.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CollabDo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeTasksController : ControllerBase
+    {
+        private readonly IEmployeeTaskService _employeeTaskService;
+
+        public EmployeeTasksController(IEmployeeTaskService employeeTaskService)
+        {
+            _employeeTaskService = employeeTaskService;
+        }
+
+
+        [HttpGet]
+        public IActionResult EmployeeTasks([FromQuery]Guid projectId) 
+        {
+            try
+            {
+                return Ok(_employeeTaskService.GetEmployeeTasks(projectId));
+            }
+            catch(ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult TaskState([FromQuery] Guid projectId, [FromQuery] Guid taskId, [FromQuery][Range(0,4)] Application.Entities.TaskStatus status)
+        {
+            try
+            {
+                return Ok(_employeeTaskService.SetTaskState(projectId,taskId,status));
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
